0821d90efaa7e6e9762bea6f59a8ba35c5314bb9
Ilya Jinx <j1nx.ed@yandex.ru>
2023-06-14 09:17:10 +0300

    fix 1

--- Homework_2/employee.sql
+++ Homework_2/employee.sql
@@ -8,20 +8,13 @@ create table if not exists Departments
 	DepName varchar(20) not null
 );
 
--- Таблица начальников
-create table if not exists Leads
-(
-	LeadId integer primary key
-);
-
 -- Таблица сотрудников
 create table if not exists Emps
 (
 	EmpId integer primary key,
 	EmpName varchar(40) not null,
-	DepId integer references Departments(DepId) unique,
-	LeadId integer references Leads(LeadId)
+	DepId integer references Departments(DepId) unique
 );
 
--- изменить таблцу начальников, добавить поле
-alter table leads add EmpId integer references Emps(EmpId) unique
+-- изменить таблцу, добавить поле
+alter table Emps add LeadId integer references Emps(EmpId)
